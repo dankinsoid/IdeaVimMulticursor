b9d7fd9ed987a6051a068d9737327b8f841a2aeb
dankinsoid <voidilov@gmail.com>
2022-05-16 17:43:54 +0300

    eeee

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,8 +66,8 @@ tasks {
 
     patchPluginXml {
         version.set(properties("pluginVersion"))
-//        sinceBuild.set(properties("pluginSinceBuild"))
-//        untilBuild.set(properties("pluginUntilBuild"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set("")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
